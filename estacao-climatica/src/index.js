b055845bb503597f98e6eeb3496b167df23f73da
Jonathan Baliellas <101955148+JonathanBaliellas@users.noreply.github.com>
2024-03-15 21:15:45 -0300

    feat(ap05): geração do construtor e método obterEstacao
    - Criação do construtor
    - Criação do método obterEstacao()

--- estacao-climatica/src/index.js
+++ estacao-climatica/src/index.js
@@ -7,6 +7,50 @@ class App extends React.Component{
     // window.navigator.geolocation.getCurrentPosition((position) => {
     //     console.log(position.coords.latitude)
     // })
+
+    constructor(props){
+        super(props)
+        this.state = {
+            latitude: null,
+            longitude: null,
+            estacao: null,
+            data: null,
+            icone: null
+        }
+    }
+
+    obterEstacao = (data, latitude) => {
+        const anoAtual = data.getFullYear()
+        // 21/06: inverno/verão
+        // new Date(ano, mes(começa do 0), dia(começa do 1))
+        const d1 = new Date(anoAtual, 5, 21)
+
+        // 24/09: primavera/outono
+        const d2 = new Date(anoAtual, 8, 24)
+
+        // 21/12: verão/inverno
+        const d3 = new Date(anoAtual, 11, 22)
+
+        // 24/03: outono/primavera
+        const d4 = new Date(anoAtual, 2, 21)
+
+        // latitude < 0 ? se sim, true; senão (:) false
+        // const sul = latitude < 0 ? true : false 
+        const sul = latitude < 0 //bool que retorna true se latitude for negativa
+
+        if (data >= d1 && data < d2) {
+            return sul ? 'Inverno' : 'Verão'
+        }else if(data >= d2 && data < d3){
+            return sul ? 'Primavera' : 'Outono'
+        }else if(data >= d3 || data < d4){
+            return sul ? 'Verão' : 'Inverno'
+        }else{
+            return sul ? 'Outono' : 'Primavera'
+        }
+
+
+    }
+
     render(){ //é preciso ter a função render, que não recebe parâmetros
         return <div>Oi</div>
     }
