5b9af7178c73153e80fe33383289efc026490fd6
Jonathan Baliellas <101955148+JonathanBaliellas@users.noreply.github.com>
2024-03-22 19:58:01 -0300

    feat(ap05): interface (encerra seção 2.7)
    - Altera o ícone de primavera
    - Implementa jsx do componente principal

--- estacao-climatica/src/index.js
+++ estacao-climatica/src/index.js
@@ -3,12 +3,12 @@ import '@fortawesome/fontawesome-free/css/all.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class App extends React.Component{
+class App extends React.Component {
     // window.navigator.geolocation.getCurrentPosition((position) => {
     //     console.log(position.coords.latitude)
     // })
 
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             latitude: null,
@@ -22,7 +22,7 @@ class App extends React.Component{
     icones = {
         'Verão': 'fa-sun',
         'Inverno': 'fa-snowflake',
-        'Primavera': 'fa-canadian-maple-leaf',
+        'Primavera': 'fa-clover',
         'Outono': 'fa-tree'
     }
 
@@ -47,11 +47,11 @@ class App extends React.Component{
 
         if (data >= d1 && data < d2) {
             return sul ? 'Inverno' : 'Verão'
-        }else if(data >= d2 && data < d3){
+        } else if (data >= d2 && data < d3) {
             return sul ? 'Primavera' : 'Outono'
-        }else if(data >= d3 || data < d4){
+        } else if (data >= d3 || data < d4) {
             return sul ? 'Verão' : 'Inverno'
-        }else{
+        } else {
             return sul ? 'Outono' : 'Primavera'
         }
     }
@@ -74,9 +74,9 @@ class App extends React.Component{
         window.navigator.geolocation.getCurrentPosition(
             (position) => {//Função callback
                 //2. Na função callback, fazer o seguinte:
-                    //- Construir um novo Date (com new Date()) que representa a data atual
+                //- Construir um novo Date (com new Date()) que representa a data atual
                 let dataAtual = new Date()
-                let {latitude, longitude} = position.coords //desestruturado
+                let { latitude, longitude } = position.coords //desestruturado
 
                 //- Chamar a função obterEstacao entregando a ela a latitude e a data atual, obtendo como resposta a estação climática
                 let estacao = this.obterEstacao(dataAtual, latitude)
@@ -103,11 +103,45 @@ class App extends React.Component{
 
             }
         )
-        
+
     }
 
-    render(){ //é preciso ter a função render, que não recebe parâmetros
-        return <div>Oi</div>
+    render() { //é preciso ter a função render, que não recebe parâmetros
+        return (
+            <div className="container mt-2">
+                <div className="row justify-content-center">
+                    <div className="col-sm-12 col-md-8">
+                        {/* Cartão do Bootstrap */}
+                        <div className="card">
+                            <div className="card-body">
+                                <div
+                                    className="d-flex align-items-center border rounded mb-2"
+                                    style={{ height: '6rem' }}
+                                    >
+                                    {/* Ícone */}
+                                    <i className={`fa-solid fa-5x ${this.state.icone}`}></i>
+                                    <p className="w-75 ms-3 text-center fs-1">{this.state.estacao}</p>
+                                </div>
+                                <div>
+                                    <p className="text-center">
+                                        {
+                                            this.state.latitude ? `Coordenadas: ${this.state.latitude}, ${this.state.longitude} . 
+                                            Data: ${this.state.data}` :
+                                            `Clique no botão para saber a sua estação climática`
+                                        }
+                                    </p>
+                                </div>
+                                <button 
+                                    className="btn btn-outline-primary w-100 mt-2"
+                                    onClick={() => this.obterLocalizacao()}>
+                                    Qual a minha estação?
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
 
