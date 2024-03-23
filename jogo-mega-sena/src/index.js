0b680de5e6df73bb0ec76c0fa0b9be419a7ac5e9
Jonathan Baliellas <101955148+JonathanBaliellas@users.noreply.github.com>
2024-03-22 21:31:43 -0300

    feat(ap05ex): implementação
    - Design da tela
    - Implementação do método gerarNum()

--- jogo-mega-sena/src/index.js
+++ jogo-mega-sena/src/index.js
@@ -1,10 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-class App extends React.Component{
-    render(){
+class App extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            num: []
+        }
+    }
+
+    gerarNum = () => {
+        for(let i = 0; i < 6; i++){
+            this.state.num[i] = Math.ceil(Math.random() * 60)
+        }
+    }
+
+    render() {
+        const cardSorteio = []
+        for(let i = 0; i < 6; i++){
+            cardSorteio.push(
+                <div className="p-4 bg-success border col-1" key={i}>{this.state.num[i]}</div>
+            )
+        }
+
         return (
-            'hello world'
+            <div className="container mt-2">
+                <div className="col-sm-12 col-md-8">
+                    <div className="card">
+                        <div className="card-header">
+                            Mega Sena
+                        </div>
+                        <div className="card-body row justify-content-center">
+                            {cardSorteio}
+                            <button className="btn btn-outline-success mt-2 col-10">
+                                Gerar jogo
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
