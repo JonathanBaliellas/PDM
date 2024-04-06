c054c528ff6e337b37636293555dd9c6d05d353e
Jonathan Baliellas <101955148+JonathanBaliellas@users.noreply.github.com>
2024-04-05 21:40:51 -0300

    feat(ap07): criação do app
    - Criação do projeto
    - Instalação e importação das bibliotecas PrimeReact, PrimeFlex e PrimeIcons
    - Importação do tema PrimeReact
    - Criação e importação do componente App.js

---
+++ busca-foto/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+
+import 'primereact/resources/primereact.css'
+import 'primereact/resources/themes/bootstrap4-light-blue/theme.css'
+import 'primeflex/primeflex.css'
+import 'primeicons/primeicons.css'
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
