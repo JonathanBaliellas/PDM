3e822064b27c985bc33aaf306b5713ef5f6c8edd
Jonathan Baliellas <101955148+JonathanBaliellas@users.noreply.github.com>
2024-05-10 20:49:20 -0300

    feat(ap001-rn): implementa o componente principal
    - Cria o componente com dois inputs que recebem valor e um botão com o método que faz a soma

--- react-native/primeiro-projeto-v2/App.tsx
+++ react-native/primeiro-projeto-v2/App.tsx
@@ -1,10 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [numberA, numA] = React.useState('')
+  const [numberB, numB] = React.useState('')
+  const [titleText, setTitleText] = useState('')
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <TextInput
+        style={styles.input}
+        placeholder='Número a'
+        onChangeText={numA}
+        value={numberA}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder='Número b'
+        onChangeText={numB}
+        value={numberB}
+      />
+      <Button
+        onPress={() => {
+          let soma = Number(numberA) + Number(numberB);
+          setTitleText(`${numberA} + ${numberB} = ${soma}`)
+        }}
+        title='Somar'
+      />
+      <Text
+        style={styles.text}
+        >{titleText}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +43,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderWidth: 1,
+    margin: 12,
+    padding: 5,
+    borderRadius: 5,
+    borderColor: 'gray'
+  },
+  text: {
+    margin: 12
+  }
 });
