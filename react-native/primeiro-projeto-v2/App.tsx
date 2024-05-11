acb678a4df3f977cbc35484d2670c2e3b7f24844
Jonathan Baliellas <101955148+JonathanBaliellas@users.noreply.github.com>
2024-05-10 21:07:03 -0300

    refactor(ap001-rn): conversão do código para classe

--- react-native/primeiro-projeto-v2/App.tsx
+++ react-native/primeiro-projeto-v2/App.tsx
@@ -1,39 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
-export default function App() {
-  const [numberA, numA] = React.useState('')
-  const [numberB, numB] = React.useState('')
-  const [titleText, setTitleText] = useState('')
+export default class App extends Component {
+  state = {
+    numA: '',
+    numB: '',
+    resultado: ''
+  }
   
-  return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder='Número a'
-        onChangeText={numA}
-        value={numberA}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder='Número b'
-        onChangeText={numB}
-        value={numberB}
-      />
-      <Button
-        onPress={() => {
-          let soma = Number(numberA) + Number(numberB);
-          setTitleText(`${numberA} + ${numberB} = ${soma}`)
-        }}
-        title='Somar'
-      />
-      <Text
-        style={styles.text}
-        >{titleText}</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder='Número a'
+          onChangeText={(num) => this.setState({numA: num})}
+          // value={numberA}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder='Número b'
+          onChangeText={(num) => this.setState({numB: num})}
+          // value={numberB}
+        />
+        <Button
+          onPress={() => {
+            const soma = Number(this.state.numA) + Number(this.state.numB);
+            this.setState({resultado: `${this.state.numA} + ${this.state.numB} = ${soma}`})
+          }}
+          title='Somar'
+        />
+        <Text
+          style={styles.text}
+          >{this.state.resultado}</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
