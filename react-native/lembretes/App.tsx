236c0fc0d909ced3778c2be377b4fbf58bd30a11
Jonathan Baliellas <101955148+JonathanBaliellas@users.noreply.github.com>
2024-05-10 22:11:09 -0300

    feat(ap002-rn): cria o componente dos lembretes

--- react-native/lembretes/App.tsx
+++ react-native/lembretes/App.tsx
@@ -1,20 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { 
+  FlatList,
+  Pressable,
+  StyleSheet, 
+  Text,
+  TextInput, 
+  View 
+} from 'react-native';
+
+interface Lembrete {
+  id: string
+  texto: string
+}
 
 export default function App() {
+  const [lembrete, setLembrete] = useState <string>('')
+  const [lembretes, setLembretes] = useState<Lembrete[]>([])
+
+  const adicionar = () => {
+    //1. construir um novo lembrete com id igual à data atual e texto igual àquilo que existe na variável de estado
+    const novoLembrete: Lembrete = {
+      id: Date.now().toString(),
+      texto: lembrete
+    }
+
+    // 2. atualizar a lista de lembretes, adicionando o novo lembrete (use a função apropriada). OBS: O último lembrete digitado deve ser o primeiro a ser exibido (top da lista)
+    setLembretes(lembretesAtual => [
+      novoLembrete,
+      ...lembretesAtual
+    ])
+
+    // 3. atualizar a variável lembrete, limpando o campo para o usuário
+    setLembrete('')
+
+    // 4. vincular essa função ao Pressable
+    //Incluído no Pressable (onPress)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        placeholder='Digite um lembrete'
+        onChangeText={setLembrete}
+        value={lembrete}
+      />
+      <Pressable
+        style={styles.button}
+        onPress={adicionar}
+        >
+        <Text
+          style={styles.buttonText}
+        >Salvar</Text>
+      </Pressable>
+      <FlatList
+        style={styles.list}
+        data={lembretes}
+        renderItem={lembrete => (
+          <View>
+            <Text>{lembrete.item.texto}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#0096F3', //material design blue 500
+    borderRadius: 4,
+    padding: 8,
+    width: '80%',
+    marginBottom: 8,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center'
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 40,
   },
+  input: {
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 4,
+    marginBottom: 12,
+    padding: 8,
+    textAlign: 'center'
+  },
+  list: {
+    borderWidth: 1,
+    borderColor: 'lightgray',
+    width: '80%',
+    borderRadius: 4,
+    padding: 8
+  }
 });
